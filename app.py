4bb7ebc2f37ba40e23c76c40360cafdb9cc51c05
Marc Farssac <marc.farssac.bts@gmail.com>
2020-03-23 06:44:10 +0100

    Updated graph1

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ app.layout = html.Div(children=[
             id='example-graph',
             figure={
                 'data': [
-                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                    {'x': [1, 2, 3], 'y': [1, 2, 3], 'type': 'bar', 'name': 'SF'},
+                    {'x': [1, 2, 3], 'y': [3, 2, 1], 'type': 'bar', 'name': u'Montréal'},
                 ],
                 'layout': {
                     'title': 'Dash Data Visualization'
